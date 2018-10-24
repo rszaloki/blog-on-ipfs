12fc0acfe578f79ec2fc7acba7b5df728efea4fb
robi <robert.szaloki@euedge.com>
2018-10-24 13:14:09 +0200

    favicon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BASE = 'QmWiXWfcJ5nxh7HhXRhnkD2evcbQZ33DxbnC4J7dnDX74c'
+const BASE = 'QmWi3eS9ZsqNbXmZuiABgPebZfFP3M61jFAt7GGYi7sVMV'
 import { getFile, getNode } from './src/server'
 
 self.addEventListener('install', (event) => {
@@ -13,11 +13,14 @@ self.addEventListener('activate', (event) => {
 })
 
 self.addEventListener('fetch', (event) => {
+  console.log(event.request.url,' ==> ')
   if (event.request.url.startsWith(self.location.origin)) {
 
     const url = new URL(event.request.url)
     const PATH = url.pathname
     const IPFS_PATH = `${BASE}${PATH}`
+
+    console.log(IPFS_PATH);
 
     event.respondWith(getFile(IPFS_PATH).then(resp => {
       const content = resp[0].content
